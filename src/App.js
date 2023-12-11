02a14be40f5a99ee22ea6a313d9c5a09f8967561
punitha-96 <punithakathirvel6@gmail.com>
2023-12-11 16:07:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import env from "react-dotenv";
+import "./App.css";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+ 
+if (!process.env.REACT_APP_VITE_CLERK_PUBLISHABLE_KEY) {
+  throw "Missing Publishable Key"
+}
+ 
+const clerkPubKey = process.env.REACT_APP_VITE_CLERK_PUBLISHABLE_KEY;
+ 
+function App() {
+  return (
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <SignedIn>
+        <Welcome />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
+  );
+}
+ 
+function Welcome() {
+  return <div>Hello you are signed in</div>;
+}
+ 
+
+export default App;
